e44cba624a664be18081c8fd14821236addfe351
Lucas Nunes Batista <lucas.batista@unemat.br>
2023-12-06 19:52:54 -0400

    fazendo corpo e cabeçalho do app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Post } from './Post'; // Importamos o componente Post
+import './styles.css' //sempre importar os estilos nos arquivos java
 
-export function App() { //impossível renderizar varios componentes sem ter outro componente que os englobe (nesse caso, div)
+export function App() { //componentes sempre com letra maiuscula para não confundir com html
+  //impossível renderizar varios componentes sem ter outro componente que os englobe (nesse caso, div)
   return (
     <div>
       <Post author="Winy Zanin"
