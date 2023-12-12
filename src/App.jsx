1a6925a10ecfea0d032a915e31128c3b4e544346
Lucas Nunes Batista <lucas.batista@unemat.br>
2023-12-11 21:31:00 -0400

    ajustando css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Post } from './Post'; // Importamos o componente Post
 import { Header } from './components/Header';
-import './styles.css' //sempre importar os estilos nos arquivos java
+import './global.css' //sempre importar os estilos nos arquivos java
 
 export function App() { //componentes sempre com letra maiuscula para não confundir com html
   //impossível renderizar varios componentes sem ter outro componente que os englobe (nesse caso, div)
