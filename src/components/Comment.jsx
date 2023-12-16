370768d5860f660870173833d79dd7384909b63a
Lucas Nunes Batista <lucas.batista@unemat.br>
2023-12-16 15:21:47 -0400

    criando e estilizando comentários

---
+++ src/components/Comment.jsx
@@ -0,0 +1,37 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment () {
+    return (
+        <div className={styles.comment}>
+            <img src="https://github.com/lucasunemat.png" alt="" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Lucas</strong>
+                            <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:30">Cerca de uma hora atrás</time>
+                        </div>
+                        <button title="Deletar comentário">
+                            <Trash size={24} />
+                        </button>
+                    </header>
+
+                    <p>Muito bom Lucas, parabéns!</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+
+            </div>
+
+
+
+        </div>
+    );
+}
