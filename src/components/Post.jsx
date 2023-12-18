1bfbd2e1f1a471a492f205371f390bac02f6222c
Lucas Nunes Batista <lucas.batista@unemat.br>
2023-12-18 18:49:20 -0400

    criação e configuração do avatar

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
@@ -7,7 +8,7 @@ export function Post() {
             <header>
                 <div className={styles.author}>
 
-                    <img className={styles.avatar}
+                    <Avatar
                         src="https://github.com/lucasunemat.png"
                         alt=""
                     />
